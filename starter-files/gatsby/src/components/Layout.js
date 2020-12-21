e15875a8dc8a9a0921e9c594971a08e2e47400ef
Kyle Hunter <kyle.hunter91@gmail.com>
2020-12-21 12:04:19 -0500

    08: Introduces Typography global styles

--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -4,11 +4,14 @@ import Footer from './Footer';
 
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
     <div>
       <GlobalStyles />
+      <Typography />
+
       <Nav />
       {children}
       <Footer />
