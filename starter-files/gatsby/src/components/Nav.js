425bf4cbdf469631d06f986770256685a8de2457
Kyle Hunter <kyle.hunter91@gmail.com>
2020-12-21 15:35:09 -0500

    09: Styles NavStyled list items and anchors

--- starter-files/gatsby/src/components/Nav.js
+++ starter-files/gatsby/src/components/Nav.js
@@ -14,6 +14,39 @@ const NavStyled = styled.nav`
 
     margin: 0;
     padding: 0;
+    list-style: none;
+  }
+
+  li {
+    --rotate: -2deg;
+
+    transform: rotate(var(--rotate));
+    order: 1;
+
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+
+    &:hover {
+      color: var(--red);
+    }
   }
 `;
 
