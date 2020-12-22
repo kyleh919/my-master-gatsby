4e8fb31d2b25f80774d03e54981914c012e519c7
Kyle Hunter <kyle.hunter91@gmail.com>
2020-12-22 15:50:15 -0500

    10: Introduces SiteBorderStyles to Layout

--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -6,6 +6,27 @@ import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
+import stripes from '../assets/images/stripes.svg';
+
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+
+  background: white url(${stripes});
+  background-size: 1500px;
+
+  padding: 5px;
+  padding: (5px, 1vw, 25px);
+  box-shadow: 4px 4px 15px 3px rgba(0, 0, 0, 0.125);
+  border: 5px solid white;
+
+  @media (max-width: 1100px) {
+    margin-right: 1.5rem;
+    margin-left: 1.5rem;
+  }
+`;
 
 const ContentStyles = styled.div`
   background: var(--white);
@@ -18,11 +39,13 @@ export default function Layout({ children }) {
       <GlobalStyles />
       <Typography />
 
-      <ContentStyles>
-        <Nav />
-        {children}
-        <Footer />
-      </ContentStyles>
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+          <Footer />
+        </ContentStyles>
+      </SiteBorderStyles>
     </div>
   );
 }
