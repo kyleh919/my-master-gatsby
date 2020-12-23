d4665e4bc8d31b0a38e79ff6fdba5a5fbb55f118
Kyle Hunter <kyle.hunter91@gmail.com>
2020-12-23 16:22:59 -0500

    13: Updates the pizza schema preview in Sanity Studio to include the pizza name, pizza image, and the first four ingredients used

--- starter-files/sanity/schemas/pizza.js
+++ starter-files/sanity/schemas/pizza.js
@@ -46,4 +46,27 @@ export default {
       of: [{ type: 'reference', to: [{ type: 'topping' }] }],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. filter the undefined toppings out
+      const definedToppings = Object.values(toppings).filter(
+        (topping) => topping !== undefined
+      );
+
+      // 2. return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: definedToppings.join(', '),
+      };
+    },
+  },
 };
