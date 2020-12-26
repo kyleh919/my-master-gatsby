3792dc868438c2920fd492549ade63dfe112b542
Kyle Hunter <kyle.hunter91@gmail.com>
2020-12-26 07:41:50 -0500

    17: Adds Gatsby plugins for styled-components and sanity

--- starter-files/gatsby/gatsby-config.js
+++ starter-files/gatsby/gatsby-config.js
@@ -1,7 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: `Slick's Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `Best pizza around!`,
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'euwjltov',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
