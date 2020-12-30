c35235a2e19449198fbb79f69be41ac6f34786ad
Kyle Hunter <kyle.hunter91@gmail.com>
2020-12-30 17:21:12 -0500

    24: Removes unused query from ToppingsFilter per Wes' suggestion

--- starter-files/gatsby/src/components/ToppingsFilter.js
+++ starter-files/gatsby/src/components/ToppingsFilter.js
@@ -56,15 +56,8 @@ function countToppingsInPizzas(pizzas) {
 export default function ToppingsFilter() {
   // 1. Get a list of all the toppings
   // 2. Get a list of all the pizzas with their toppings
-  const { toppings, pizzas } = useStaticQuery(graphql`
+  const { pizzas } = useStaticQuery(graphql`
     query MyQuery {
-      toppings: allSanityTopping {
-        nodes {
-          id
-          name
-          vegetarian
-        }
-      }
       pizzas: allSanityPizza {
         nodes {
           toppings {
