adc45d33717c20c48426f9e78769f277d66dd64c
Kyle Hunter <kyle.hunter91@gmail.com>
2020-12-30 17:11:51 -0500

    24: My attempt at styling the SinglePizzaPage

--- starter-files/gatsby/src/templates/Pizza.js
+++ starter-files/gatsby/src/templates/Pizza.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function SinglePizzaPage() {
-  return <p>I'm a single pizza page!</p>;
+const SinglePizzaStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+`;
+
+export default function SinglePizzaPage({ data }) {
+  const { pizza } = data;
+
+  return (
+    <SinglePizzaStyles>
+      <Img fluid={pizza.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li>{topping.name}</li>
+          ))}
+        </ul>
+      </div>
+    </SinglePizzaStyles>
+  );
 }
 
 // this needs to be dynamic based on the slug passed in via
