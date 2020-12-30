6e12874e29e1a7ec5297780450d5330f47d5c8f2
Kyle Hunter <kyle.hunter91@gmail.com>
2020-12-30 14:39:10 -0500

    23: Creates GraphQL query for the selected pizzas Sanity data in the generic Pizza template

--- starter-files/gatsby/src/templates/Pizza.js
+++ starter-files/gatsby/src/templates/Pizza.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 export default function SinglePizzaPage() {
   return <p>I'm a single pizza page!</p>;
 }
+
+// this needs to be dynamic based on the slug passed in via
+// the context in gatsby-node.js
+export const pizzaQuery = graphql`
+  query($slug: String!) {
+    pizza: sanityPizza(slug: { current: { eq: $slug } }) {
+      name
+      id
+      toppings {
+        name
+        id
+        vegetarian
+      }
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+    }
+  }
+`;
