14eb701bf8ffe8e97cadf741fc6863959d84e2ed
Kyle Hunter <kyle.hunter91@gmail.com>
2020-12-30 12:37:55 -0500

    23: Includes dynamic page creation for queried pizzas

--- starter-files/gatsby/gatsby-node.js
+++ starter-files/gatsby/gatsby-node.js
@@ -1,6 +1,6 @@
 import path from 'path';
 
-async function turnPizzasIntoPages({ graphql }) {
+async function turnPizzasIntoPages({ graphql, actions }) {
   // 1. Get a template for this page
   const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
   // 2. Query all pizzas
@@ -20,6 +20,20 @@ async function turnPizzasIntoPages({ graphql }) {
   console.log(data);
 
   // 3. Loop over each pizza and create a page for that pizza
+  data.pizzas.nodes.forEach((pizza) => {
+    console.log('Creating a pizza page for ', pizza.name);
+
+    actions.createPage({
+      // URL of the page
+      path: `pizza/${pizza.slug.current}`,
+      // Component to display on the page
+      component: pizzaTemplate,
+      // Any data you want to pass down to the given component
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
 }
 
 export async function createPages(params) {
