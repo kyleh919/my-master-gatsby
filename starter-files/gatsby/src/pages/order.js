325c0e9d4260ce237439504c52c8ce412a815097
Kyle Hunter <kyle.hunter91@gmail.com>
2021-01-09 14:11:06 -0800

    34: Queries for pizzas in Sanity and then displays the name, image, and prices

--- starter-files/gatsby/src/pages/order.js
+++ starter-files/gatsby/src/pages/order.js
@@ -1,13 +1,19 @@
+import { graphql } from 'gatsby';
 import React, { useState } from 'react';
+import Img from 'gatsby-image';
 import SEO from '../components/SEO';
 import useForm from '../utils/useForm';
+import calculatePizzaPrice from '../utils/calculatePizzaPrice';
+import formatMoney from '../utils/formatMoney';
 
-export default function OrderPage() {
+export default function OrderPage({ data }) {
   // const [name, setName] = useState('');
   const { values, updateValue } = useForm({
     name: '',
     email: '',
   });
+
+  console.log(data);
 
   return (
     <>
@@ -36,6 +42,27 @@ export default function OrderPage() {
 
         <fieldset>
           <legend>Menu</legend>
+
+          {data.pizzas.nodes.map((pizza) => (
+            <div key={pizza.id}>
+              <Img
+                width="50"
+                height="50"
+                fluid={pizza.image.asset.fluid}
+                alt={pizza.name}
+              />
+              <div>
+                <h3>{pizza.name}</h3>
+              </div>
+              <div>
+                {['S', 'M', 'L'].map((size) => (
+                  <button type="button">
+                    {size} {formatMoney(calculatePizzaPrice(pizza.price, size))}
+                  </button>
+                ))}
+              </div>
+            </div>
+          ))}
         </fieldset>
 
         <fieldset>
@@ -45,3 +72,22 @@ export default function OrderPage() {
     </>
   );
 }
+
+export const query = graphql`
+  query OrderQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        price
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
