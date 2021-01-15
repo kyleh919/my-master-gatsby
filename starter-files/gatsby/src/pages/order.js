344f1932ad3f969adce680d408620c0f27675ce5
Kyle Hunter <kyle.hunter91@gmail.com>
2021-01-15 15:29:03 -0800

    37: Adds calculateOrderTotal to the order page

--- starter-files/gatsby/src/pages/order.js
+++ starter-files/gatsby/src/pages/order.js
@@ -9,6 +9,7 @@ import OrderStyles from '../styles/OrderStyles';
 import MenuItemStyles from '../styles/MenuItemStyles';
 import usePizza from '../utils/usePizza';
 import PizzaOrder from '../components/PizzaOrder';
+import calculateOrderTotal from '../utils/calculateOrderTotal';
 
 export default function OrderPage({ data }) {
   const pizzas = data.pizzas.nodes;
@@ -88,6 +89,14 @@ export default function OrderPage({ data }) {
             removeFromOrder={removeFromOrder}
             pizzas={pizzas}
           />
+        </fieldset>
+        <fieldset>
+          <legend>Order Total</legend>
+          <h3>
+            Your Order Total is{' '}
+            {formatMoney(calculateOrderTotal(order, pizzas))}
+          </h3>
+          <button type="submit">Order Ahead</button>
         </fieldset>
       </OrderStyles>
     </>
